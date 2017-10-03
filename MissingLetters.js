aafbee29bdfacec00b6ef5c72869661ce7daaee3
Pranav Kural <pranavkural042@gmail.com>
2017-10-03 12:59:21 -0400

    Missing letters in the alphabetic order

---
+++ MissingLetters.js
@@ -0,0 +1,17 @@
+// Missing letters
+/*
+Find the missing letter in the passed letter range and return it.
+If all letters are present in the range, return undefined.
+*/
+
+function fearNotLetter(str) {
+  var start = str.charCodeAt(0);
+  for (var i = 1; i < str.length; i++) {
+    if (str[i] !== String.fromCharCode(++start))
+      return String.fromCharCode(start);
+  }
+  
+  return undefined;
+}
+
+fearNotLetter("abce");
