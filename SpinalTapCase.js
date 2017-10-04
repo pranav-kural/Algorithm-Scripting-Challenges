c27ad6decfb8e7332ce86e7d1a1f6bf8052d3535
Pranav Kural <pranavkural042@gmail.com>
2017-10-04 11:31:33 -0400

    Added comments

--- SpinalTapCase.js
+++ SpinalTapCase.js
@@ -8,4 +8,9 @@ function spinalCase(str) {
   }).replace(/([_])|([^\w])/g, "-");
 }
 
+// What's happening here:
+  //1. Replace all Capital Case letter to lower case
+  //2. if the Capital case letter has a letter or number on it's left add a "-" between them
+  //3. replace all non-alphanumeric characters (including spaces) with "-"
+  
 spinalCase('This Is Spinal Tap');
