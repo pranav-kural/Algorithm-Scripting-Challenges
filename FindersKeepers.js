4a33819b37ca820f389d093809e360fc55abf606
Pranav Kural <pranavkural042@gmail.com>
2017-10-09 01:25:29 -0400

    Finders Keepers

---
+++ FindersKeepers.js
@@ -0,0 +1,11 @@
+// Finders Keepers
+
+/* Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).*?
+
+function findElement(arr, func) {
+  return arr.filter((val) => {
+    return func(val);
+  })[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
